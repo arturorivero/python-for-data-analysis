78316281dfb1f2280c100b7b6bdfc70817949392
AlufaSam <105754974+AlufaSam@users.noreply.github.com>
2024-04-21 22:26:28 +0000

    adds main.ipynb and main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import plotly.graph_objs as go
+from plotly.offline import iplot
+ #Reading dataset
+data = pd.read_csv("data/BankChurners_v2.csv")
